0e9fa7b31b809859ded2db56ec936948c467ae80
GIT-justin12 <agbozojustin42@gmail.com>
2023-08-10 12:25:37 +0000

    Redaction of fonctions: chooseWordOrSentence, startLoop, printScore and startGame.

---
+++ azerType/main.js
@@ -0,0 +1,5 @@
+/**
+ * Jeu de saisir.
+ */
+
+startGame()
--- azerType/script.js
+++ azerType/script.js
@@ -5,18 +5,61 @@
 
 /**
  * chooseWordOrSentence - let use to choose his choice.
- * @return {choice} return the choix of the user.
+ * @return {string} return the choix of the player.
  */
 function chooseWordOrSentence() {
     let choice 
-    
+
     do {
-        choice = prompt("Avec quel liste désirez-vous jouer?: \"mots\" ou \"phrases\"")
+        choice = prompt("Avec quel liste désirez-vous jouer?: \"mot\" ou \"phrase\"")
     }
-    while (choice !== "mots" && choice !== "phrases")
+    while (choice !== "mot" && choice !== "phrase")
 
     return choice
 }
-let test
-test = chooseWordOrSentence()
-console.log(test)
+
+/**
+ * startLoop - start a loop of preposition according to choice and increment a score.
+ * @param {Array[string]} list of proposition.
+ * @return {number} the score of the player.
+ */
+function startLoop(propositionList) {
+    let score = 0
+    for (let i = 0; i < propositionList.length; i++) {
+        let userProposition = prompt("Saisir: "+ propositionList[i]);
+        if (propositionList[i] === userProposition) {
+            score++
+        }
+    }
+    return score
+}
+
+/**
+ * printScore - Print score.
+ * @param {number} number of propositions.
+ * @param {number} the score of player.
+ */
+function printScore(score, nbProposition) {
+    console.log("Votre score: " + score + " sur " + nbProposition + " propositions.")
+}
+
+/**
+ * startGame - Start the game.
+ */
+function startGame() {
+    let choice =  chooseWordOrSentence()
+    let score = 0
+    let mode = 0
+    let nbProposition = 0
+
+    if (choice === "mot") {
+        score = startLoop(wordsList)
+        nbProposition = wordsList.length
+    } else {
+        mode = 1
+        score = startLoop(sentencesList)
+        nbProposition = sentencesList.length
+    }
+
+    printScore(score, nbProposition)
+}
